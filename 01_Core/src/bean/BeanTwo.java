6921d52a40ae21f60b9fe1e52cad84780d5dc024
oshadaera68 <mindartlk010@gmail.com>
2022-06-25 15:31:29 +0530

    updated content

---
+++ 01_Core/src/bean/BeanTwo.java
@@ -0,0 +1,15 @@
+/**
+ * @Owner - Oshada Eranga
+ * @Version - v0.1.0
+ **/
+
+package bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanTwo {
+    public BeanTwo() {
+        System.out.println("Bean two object");
+    }
+}
