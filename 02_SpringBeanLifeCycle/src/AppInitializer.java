905fede9f9f065e9613a48f4f831d8adef673237
oshadaera68 <mindartlk010@gmail.com>
2022-06-28 22:25:17 +0530

    updated

--- 02_SpringBeanLifeCycle/src/AppInitializer.java
+++ 02_SpringBeanLifeCycle/src/AppInitializer.java
@@ -1,4 +1,3 @@
-import bean.SpringBean;
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,11 +13,11 @@ public class AppInitializer {
         context.refresh();
         context.registerShutdownHook();
 
-        SpringBean bean = context.getBean(SpringBean.class);
-        SpringBean bean2 = context.getBean(SpringBean.class);
-        SpringBean bean3 = context.getBean(SpringBean.class);
-        System.out.println(bean);
-        System.out.println(bean2);
-        System.out.println(bean3);
+        //SpringBean bean = context.getBean(SpringBean.class);
+        //SpringBean bean2 = context.getBean(SpringBean.class);
+        //SpringBean bean3 = context.getBean(SpringBean.class);
+        // System.out.println(bean);
+        //System.out.println(bean2);
+        //System.out.println(bean3);
     }
 }
--- 02_SpringBeanLifeCycle/src/config/AppConfig.java
+++ 02_SpringBeanLifeCycle/src/config/AppConfig.java
@@ -5,10 +5,18 @@
 
 package config;
 
+import bean.MyConnection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "bean")
 public class AppConfig {
+
+    @Bean
+    public MyConnection myConnection(){
+        return new MyConnection();
+    }
+
 }
